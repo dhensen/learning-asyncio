4d2edf7b3eea0885e5eec5851f8e7208bada9940
Dino Hensen <dino.hensen@gmail.com>
2018-03-09 10:20:51 +0100

    example: asyncio example of subprocess. executing sleep to simulate long running stuff

---
+++ 4.py
@@ -0,0 +1,31 @@
+import asyncio
+import subprocess
+
+
+async def run(command, timeout=None):
+    process = await asyncio.create_subprocess_exec(
+        *command,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+    return await process.wait()
+
+
+async def hello():
+    print('hello')
+    # await asyncio.sleep(3)
+    res = await run(['/usr/bin/sleep', '3'])
+    print('world')
+
+
+async def doit(loop):
+    """
+    Pass in the loop and call create_task instead of asyncio.ensure_future
+    """
+    tasks = [hello() for i in range(1, 10)]
+    await asyncio.wait(tasks)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(doit(loop))
