76f10cfdeda0388475e70541108afbb02b5ff1fd
Dino Hensen <dino.hensen@gmail.com>
2022-04-27 22:14:08 +0200

    fix typo

--- 5.py
+++ 5.py
@@ -2,7 +2,7 @@ import asyncio
 import subprocess
 
 
-async def wait_for_starts_to_align(condition):
+async def wait_for_stars_to_align(condition):
     print('waiting for the stars to be aligned')
     async with condition:
         print('the stars are aligned')
@@ -24,7 +24,7 @@ async def doit(loop):
     condition = asyncio.Condition()
     return await asyncio.wait([
         align_the_stars(condition),
-        wait_for_starts_to_align(condition),
+        wait_for_stars_to_align(condition),
     ])
 
 
