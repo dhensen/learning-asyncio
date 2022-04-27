7c309b967c2ded18f60e145a6249566cf993a4df
Dino Hensen <dino.hensen@gmail.com>
2022-04-27 23:18:36 +0200

    Use boundedSemaphore via contextvars. If you try to use a basic global var, it wont work as asyncio.run starts its own new event loop, while BoundedSemaphore takes the existing event loop, ending up with two event loops, which doesnt work, alternative could be passing along semaphore via asyncio.run main function, but I dont like passing it along everywhere

--- nasa.py
+++ nasa.py
@@ -1,6 +1,7 @@
 # Astronomy Picture Of the Day
 
 import asyncio
+import contextvars
 import logging
 import os
 import time
@@ -13,8 +14,9 @@ import httpx
 API_KEY = os.getenv('NASA_API_KEY')
 NASA_APOD_API = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
 
-logging.basicConfig(level=logging.INFO)
+sema = contextvars.ContextVar('sema')
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -32,6 +34,7 @@ def duration(func):
 
 @duration
 async def get_pictures(count=1):
+    sema.set(asyncio.BoundedSemaphore(value=10))
     async with httpx.AsyncClient() as client:
         logger.info(f'get {NASA_APOD_API}')
         response = await client.get(NASA_APOD_API,
@@ -40,24 +43,22 @@ async def get_pictures(count=1):
         response.raise_for_status()
         tasks = []
         # print(response.content)
-        for item in response.json():
-            img_url = item['url']
-            if 'https://apod.nasa.gov/apod/image/' not in img_url:
-                continue  # apod also serves youtube urls
-            # tasks.append(get_image(client, img_url))
-            tasks.append(get_and_save(client, img_url))
-        await asyncio.wait(tasks)
-        # tasks2 = []
-        # for coro in asyncio.as_completed(tasks):
-        #     img_name, img_bytes = await coro
-        #     tasks2.append(save_images_async(img_name, img_bytes))
-        # await asyncio.wait(tasks2)
+        try:
+            for item in response.json():
+                img_url = item['url']
+                if 'https://apod.nasa.gov/apod/image/' not in img_url:
+                    continue  # apod also serves youtube urls
+                tasks.append(get_and_save(client, img_url))
+            await asyncio.wait(tasks)
+        except Exception as exc:
+            logger.exception(f'exception occurred')
 
 
 @duration
 async def get_and_save(client, img_url):
-    img_name, img_response = await get_image(client, img_url)
-    await save_images_async(img_name, img_response)
+    async with sema.get():
+        img_name, img_response = await get_image(client, img_url)
+        await save_images_async(img_name, img_response)
 
 
 async def get_image(client, img_url: str):
@@ -76,4 +77,4 @@ async def save_images_async(img_name, img_bytes):
 if __name__ == "__main__":
     rmtree('nasa_apod')
     makedirs('nasa_apod', exist_ok=True)
-    asyncio.run(get_pictures(count=100))
+    asyncio.run(get_pictures(count=50))
